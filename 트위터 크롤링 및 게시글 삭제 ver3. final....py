c1ec01842870c503f60b540b92a44e695699c232
박유미 <81951313+byumm315@users.noreply.github.com>
2023-07-23 20:32:49 +0900

    Update 트위터 크롤링 및 게시글 삭제 ver3. final....py

--- 트위터 크롤링 및 게시글 삭제 ver3. final....py
+++ 트위터 크롤링 및 게시글 삭제 ver3. final....py
@@ -103,7 +103,7 @@ while True:
     button5location = pyautogui.locateOnScreen("myScreenshot.png",confidence=0.9) 
     button6location = pyautogui.locateOnScreen("myScreenshotkk.png",confidence=0.9)
     if button5location==None and button6location==None:
-        pyautogui.scroll(-500)
+        pyautogui.scroll(-400)
         continue
     elif button6location==None:
         pyautogui.scroll(-100)
@@ -117,7 +117,7 @@ while True:
     if len(cc)>0:
         if 'Del' in cc[0].text:
             ActionChains(driver).move_to_element(cc[0]).click(cc[0]).perform()
-            cc=driver.find_elements(By.CSS_SELECTOR,'#layers > div:nth-child(2) > div > div > div > div > div > div.css-1dbjc4n.r-1awozwy.r-1kihuf0.r-18u37iz.r-1pi2tsx.r-1777fci.r-1pjcn9w.r-xr3zp9.r-1xcajam.r-ipm5af.r-9dcw1g > div.css-1dbjc4n.r-z6ln5t.r-14lw9ot.r-1867qdf.r-1jgb5lz.r-pm9dpa.r-1rnoaur.r-494qqr.r-13qz1uu > div.css-1dbjc4n.r-eqz5dr.r-1hc659g.r-1n2ue9f.r-11c0sde.r-13qz1uu > div:nth-child(1) > div > span > span')
+            cc=driver.find_elements(By.CSS_SELECTOR,'#layers > div:nth-child(2) > div > div > div > div > div > div.css-1dbjc4n.r-1awozwy.r-1kihuf0.r-18u37iz.r-1pi2tsx.r-1777fci.r-1pjcn9w.r-xr3zp9.r-1xcajam.r-ipm5af.r-9dcw1g > div.css-1dbjc4n.r-z6ln5t.r-14lw9ot.r-1867qdf.r-1jgb5lz.r-1rnoaur.r-494qqr.r-13qz1uu > div.css-1dbjc4n.r-eqz5dr.r-1hc659g.r-1n2ue9f.r-11c0sde.r-13qz1uu > div.css-18t94o4.css-1dbjc4n.r-42olwf.r-sdzlij.r-1phboty.r-rs99b7.r-16y2uox.r-6gpygo.r-peo1c.r-1ps3wis.r-1ny4l3l.r-1udh08x.r-1guathk.r-1udbk01.r-o7ynqc.r-6416eg.r-lrvibr.r-3s2u2q > div > span > span')
             ActionChains(driver).move_to_element(cc[0]).click(cc[0]).perform()
             time.sleep(4)
             continue
